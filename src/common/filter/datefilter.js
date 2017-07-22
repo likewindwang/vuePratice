138b124e092a105b17aa3d96699d84422412460c
webyan <737809475@qq.com>
2017-07-22 10:46:35 +0800

    时间过滤器配置成全局

--- src/common/filter/datefilter.js
+++ src/common/filter/datefilter.js
@@ -1,3 +1,18 @@
 /**
  * Created by wangyanqing on 2017/7/22.
  */
+
+//时间过滤器
+
+//引入vue 
+import Vue from 'vue'
+
+//引入moment
+import moment from 'moment'
+
+export default Vue.filter('datefilter',function(data,format){
+    //按照年月日 时分秒计算格式YYYY-MM-DD HH：mm:ss
+    return moment(data).format(format||'YYYY-MM-DD HH:mm:ss');
+});
+
+
